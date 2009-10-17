909adc5e994df28b416909b2f266581f6bcbee61
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-10-17 15:04:32 +0900

    remove old style step definitions

--- core/features/step_definitions/tdiary_steps.rb
+++ core/features/step_definitions/tdiary_steps.rb
@@ -1,20 +1,4 @@
 # -*- coding: utf-8 -*-
-require 'nokogiri'
-
-Given "最低限のtdiary.conf" do
-	fixture_dir = File.expand_path("../fixtures", File.dirname(__FILE__))
-	work_data_dir = File.expand_path("../data", File.dirname(__FILE__))
-	sources = Dir.glob("#{fixture_dir}/just_installed/data/*")
-
-	unless sources.empty?
-		FileUtils.cp_r sources, work_data_dir, :verbose => true
-	end
-
-	@driver = TDiaryDriver.configure do
-		data_path File.expand_path(
-			"../fixtures/just_installed/tdiary.conf", File.dirname(__FILE__))
-	end
-end
 
 Given "CGIが最低限動く設定" do
 	fixture_dir = File.expand_path("../fixtures", File.dirname(__FILE__))
@@ -24,33 +8,6 @@ Given "CGIが最低限動く設定" do
 	unless sources.empty?
 		FileUtils.cp_r sources, work_data_dir, :verbose => false
 	end
-end
-
-
-# FIXME paramのハンドリングをもう少しまともにやること
-Given "デフォルトの基本設定項目" do
-	@driver.append_param("html_title=")
-#	@driver.append_params("author_name")
-	@driver.append_param("author_mail=")
-	@driver.append_param("index_page=")
-	@driver.append_param("description=")
-	@driver.append_param("icon=")
-	@driver.append_param("banner=")
-	@driver.append_param("base_url=")
-end
-
-Given /(?:クエリ|フォーム)パラメータは (.*)/ do |param|
-	@driver.append_param(param)
-end
-
-When /(.*) に(?:アクセス|ポスト)した/ do |uri|
-	target = case uri
-				when "index.rb"; :index
-				when "update.rb"; :update
-				else raise "invalid uri: must be index.rb or update.rb"
-				end
-	@status, @header, @response = @driver.invoke(target)
-	@response = Nokogiri(@response)
 end
 
 Then /ステータスコードは (.*) である/ do |status|
