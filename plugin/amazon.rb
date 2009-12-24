1344ac43459bddae7c8ef44b5acbb1fb11939225
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2009-12-24 03:30:28 +0000

    supported EAN code. Thanks for yoshida.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk@3559 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/amazon.rb
+++ plugin/amazon.rb
@@ -60,6 +60,7 @@ def amazon_call_ecs( asin, id_type, country = nil )
 	url << "&ItemId=#{asin}"
 	url << "&IdType=#{id_type}"
 	url << "&SearchIndex=Books" if id_type == 'ISBN'
+	url << "&SearchIndex=All"   if id_type == 'EAN'
 	url << "&ResponseGroup=Medium"
 	url << "&Version=#{@amazon_require_version}"
 
@@ -230,7 +231,7 @@ def amazon_get( asin, with_image = true, label = nil, pos = 'amazon' )
 	digit = asin.gsub( /[^\d]/, '' )
 	if digit.length == 13 then # ISBN-13
 		asin = digit
-		id_type = 'ISBN'
+		id_type = /^97[89]/ =~ digit ? 'ISBN' : 'EAN'
 	else
 		id_type = 'ASIN'
 	end
